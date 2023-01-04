b97120ed01db1251d1ec8e55b40cfdfa221052bc
Menesesp20 <77457105+Menesesp20@users.noreply.github.com>
2023-01-04 15:30:32 +0000

    Update Game.py

--- Functions/Game.py
+++ Functions/Game.py
@@ -1120,7 +1120,7 @@ def heatMap_xT(club, league, gameDay, player=None):
         else:
                 xTheatMap = xTDF.loc[(xTDF['name'] == player) & (xTDF['Match_ID'] == gameDay)]
 
-        fig, ax = plt.subplots(figsize=(10, 15))
+        fig, ax = plt.subplots(figsize=(10, 15), dpi=300)
 
         pitch = Pitch(pitch_type='opta',
                       pitch_color='#E8E8E8', line_color='#181818',

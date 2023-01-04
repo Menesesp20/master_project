5f35ec93d48b5633058763698df4f79b8c013ae8
Menesesp20 <77457105+Menesesp20@users.noreply.github.com>
2023-01-04 17:00:42 +0000

    Update xT.py

--- Functions/xT.py
+++ Functions/xT.py
@@ -14,12 +14,6 @@ from highlight_text import  ax_text, fig_text
 
 from soccerplots.utils import add_image
 
-from pandas.core.common import SettingWithCopyWarning
-
-import warnings
-warnings.simplefilter(action="ignore", category=SettingWithCopyWarning)
-
-
 #############################################################################################################################################################
 
 import matplotlib.pyplot as plt

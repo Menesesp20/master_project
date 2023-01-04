d77d36a87d1501d8ad9da02e555430f12f0b67f0
Menesesp20 <77457105+Menesesp20@users.noreply.github.com>
2023-01-04 16:40:01 +0000

    update

--- Functions/data.py
+++ Functions/data.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 import streamlit as st
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def getDataOPTA():
     eventsPlayers = pd.read_csv('Data/opta/optaData.csv')
     return eventsPlayers
 
-@st.cache
+@st.cache(allow_output_mutation=True)
 def getDataWyScout():
     wyScout = pd.read_csv('Data/WyScout/WyScout.csv')
     return wyScout
